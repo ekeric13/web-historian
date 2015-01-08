d65d8ca0f5f30ce56efc73022a44970d40e50703
Eric Kennedy <ekeric13@gmail.com>
2015-01-07 17:24:30 -0800

    finished test suite

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -10,10 +10,49 @@ exports.headers = headers = {
   'Content-Type': "text/html"
 };
 
+exports.sendResponse = function(res, data, status) {
+  status = status || 200;
+  res.writeHead(status, exports.headers);
+  res.end(JSON.stringify(data));
+}
+
+// asset = url.parse(req.url).pathname
 exports.serveAssets = function(res, asset, callback) {
   // Write some code here that helps serve up your static files!
-  // (Static files are things like html (yours or archived from others...), css, or anything that doesn't change often.)
+  // console.log("Asset "+ asset);
+  if (asset === "/"){
+    asset = "/index.html"
+  }
+  var path = archive.paths.siteAssets + asset;
+  // console.log("path "+ path);
+  var encoding = {encoding:'utf8'};
+  fs.readFile(path, encoding, function(err,data){
+    if(err){
+      // console.log("data stored path "+ path)
+      path = archive.paths.archivedSites + asset
+      fs.readFile(path, encoding, function(err, data){
+        if (err){
+          exports.sendResponse(res, err, 404);
+        } else {
+          exports.sendResponse(res, data, 200);
+        }
+      })
+    } else {
+      exports.sendResponse(res, data, 200);
+    }
+  })
 };
+
+exports.collectData = function(request, callback) {
+  var data = "";
+  request.on('data', function(chunk){
+    data += chunk;
+  });
+  request.on('end', function(){
+    callback(data);
+  })
+}
+
 
 
 
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,41 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+var httpHelpers = require("./http-helpers.js");
+var data = '<input>';
+var url = require("url")
+
+var actions = {
+  "GET" : function(request, response){
+    var path = url.parse(request.url).pathname;
+    httpHelpers.serveAssets(response, path);
+  },
+  "POST" : function(request, response){
+    // first check to see if it is in the list of URL's
+
+      // if true then add to list of urls
+        // also write to the archives
+    // archive.readListOfUrls
+    httpHelpers.collectData(request, function(dataUrl){
+      var parsedUrl = dataUrl.split("=")[1] + "\n";
+      if(!archive.isUrlInList(parsedUrl)){
+        archive.addUrlToList(parsedUrl);
+        httpHelpers.sendResponse(response, "url added to list", 302)
+      } else {
+        httpHelpers.sendResponse(response, "url already in list", 404)
+      }
+
+    });
+  }
+
+}
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  var action = actions[req.method];
+  if (action){
+    action(req, res)
+  } else {
+    httpHelpers.sendResponse(res, "not found", 404)
+  }
+  // res.end(archive.paths.list);
 };
